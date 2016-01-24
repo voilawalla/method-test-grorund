e4a15d51630acc3b6c697fe992c73e849b6eb90d
voilawalla <voilawalla411@gmail.com>
2016-01-24 17:08:11 -0600

    Add Gemfile and initial launch file

---
+++ main.rb
@@ -0,0 +1,10 @@
+#Input: text file with target price and available menu items
+#Output: All combinations that match target price exactly
+
+#Prompt user for menu file/location
+puts 'Please enter the relative filename containing your menu text: '
+customer_menu = gets.chomp
+
+#Return combinations that match text file target
+menu_list = Menu.new(customer_menu)
+puts menu_list.show_combos
